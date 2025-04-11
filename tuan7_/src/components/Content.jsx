72d107f7d26c5897d4f48e53f8da065b4145b14a
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-11 17:08:29 +0700

    4. Connect API - Datatable (get)

--- tuan7_/src/components/Content.jsx
+++ tuan7_/src/components/Content.jsx
@@ -1,15 +1,30 @@
 import CustomerTable from './CustomerTable';
 import Overview from './Overview';
+import { useState, useEffect } from 'react';
 
 function Content() {
+    const [customers, setCustomers] = useState([]);
+    const [selectedRows, setSelectedRows] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/customers')
+            .then((res) => res.json())
+            .then((data) => setCustomers(data))
+            .catch((error) => console.error('Error fetching customers:', error));
+    }, []);
+    console.log("Customers:", customers);
+
     return (
         <div className="content p-3">
             <div className="h-40">
                 <Overview />
             </div>
-            <div>
-                <CustomerTable />
-            </div>
+            <CustomerTable
+                customers={customers}
+                setCustomers={setCustomers}
+                selectedRows={selectedRows}
+                setSelectedRows={setSelectedRows}
+            />
         </div>
     );
 }
