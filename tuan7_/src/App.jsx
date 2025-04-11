ce89b80fbab4e69c3ddb630b0fdc6755339986cc
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-11 16:42:20 +0700

    2. OverView + Connect 3 API

--- tuan7_/src/App.jsx
+++ tuan7_/src/App.jsx
@@ -1,8 +1,12 @@
 import Menu from './components/Menu'
 import Header from './components/Header'
 import Content from './components/Content'
+import { useState } from 'react';
+
 
 function App() {
+  const [customers, setCustomers] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
   return (
     <>
       <div className="container">
@@ -13,7 +17,11 @@ function App() {
           <Header />
         </div>
         <div className="content">
-          <Content />
+          <Content
+            customers={customers}
+            setCustomers={setCustomers}
+            selectedRows={selectedRows}
+            setSelectedRows={setSelectedRows} />
         </div>
       </div>
     </>
