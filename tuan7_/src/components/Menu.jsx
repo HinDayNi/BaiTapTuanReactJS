75182e6fd4c85505c65d65c2e90ccd81095e21c1
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-11 17:22:04 +0700

    6. isActive (NavLink React Router)

--- tuan7_/src/components/Menu.jsx
+++ tuan7_/src/components/Menu.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
 function Menu() {
     return (
         <>
@@ -38,5 +37,4 @@ function Menu() {
         </>
     );
 }
-
 export default Menu;
