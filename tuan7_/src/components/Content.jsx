67b8f37693ef98adca1fb961dcf4fb129acd356b
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-11 16:46:04 +0700

    3. Datatable

--- tuan7_/src/components/Content.jsx
+++ tuan7_/src/components/Content.jsx
@@ -1,11 +1,14 @@
+import CustomerTable from './CustomerTable';
 import Overview from './Overview';
 
 function Content() {
-
     return (
         <div className="content p-3">
             <div className="h-40">
                 <Overview />
+            </div>
+            <div>
+                <CustomerTable />
             </div>
         </div>
     );
