774277630f91b951c5f29967f22c311dec12c961
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-16 21:13:50 +0700

    tuan08

--- tuan08/src/App.jsx
+++ tuan08/src/App.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.css'
-import Content from './components/Content'
-import Header from './components/Header'
-import Footer from './components/common/Footer'
+    import React from 'react';
+    import './App.css'
+    import Content from './components/Content'
+    import Header from './components/Header'
+    import Footer from './components/common/Footer'
 
-function App() {
+    function App() {
 
-  return (
-    <div className="flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Content />
-      </main>
-      <Footer />
-    </div>
-  )
-}
+      return (
+        <div className="flex flex-col">
+          <Header />
+          <main className="flex-1">
+            <Content />
+          </main>
+          <Footer />
+        </div>
+      )
+    }
 
-export default App
+    export default App
