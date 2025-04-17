d892a2399c7c24ef59b301cc5ec82bc192434a05
FyNgg04 <ngthithanhhien04@gmail.com>
2025-04-17 18:22:38 +0700

    updatenameupdatename

--- tuan09/src/main.jsx
+++ tuan09/src/main.jsx
@@ -1,16 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { store } from './app/store/store.js';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
 
-import { Provider } from 'react-redux'
-
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <App />
-      
-    </StrictMode>
-  </Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
